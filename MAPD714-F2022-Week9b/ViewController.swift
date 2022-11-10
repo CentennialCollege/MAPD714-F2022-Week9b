64e6d49cd27d42ac30c90257ee80abe0e28b1fdc
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-09 19:47:32 -0500

    created UI v1

--- MAPD714-F2022-Week9b/ViewController.swift
+++ MAPD714-F2022-Week9b/ViewController.swift
@@ -1,14 +1,67 @@
-
 import UIKit
 
-class ViewController: UICollectionViewController
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout
 {
-
-    override func viewDidLoad() {
+    // instance members
+    private var sections = [
+            ["header": "First Witch",
+             "content" : "Hey, when will the three of us meet up later?"],
+            ["header" : "Second Witch",
+             "content" : "When everything's straightened out."],
+            ["header" : "Third Witch",
+             "content" : "That'll be just before sunset."],
+            ["header" : "First Witch",
+             "content" : "Where do you want to meet?"],
+            ["header" : "Second Witch",
+             "content" : "The dirt patch."],
+            ["header" : "Third Witch",
+             "content" : "I guess we'll see Mac there."]
+            ]
+    
+    // life cycle function for UIKit
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
+        self.collectionView?.register(ContentCell.self, forCellWithReuseIdentifier: "CONTENT")
+        var contentInset = collectionView!.contentInset
+        contentInset.top = 20
+        collectionView!.contentInset = contentInset
+        
     }
-
-
+    
+    func wordsInSection(section: Int) -> [String]
+    {
+        let content = sections[section]["content"]
+        let spaces = NSCharacterSet.whitespacesAndNewlines
+        let words = content?.components(separatedBy: spaces)
+        
+        return words!
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int
+    {
+         return sections.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        let words = wordsInSection(section: section)
+        
+        return words.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let words = wordsInSection(section: indexPath.section)
+        
+        // creates a new Collection View Cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CONTENT", for: indexPath) as! ContentCell
+        
+        cell.maxWidth = collectionView.bounds.size.width
+        cell.text = words[indexPath.row]
+        
+        return cell
+    }
 }
 
